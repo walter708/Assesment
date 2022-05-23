e3bf86a652183e1d56657929170983e57392bb5a
unknown <Walter Okpala>
2022-05-22 22:02:36 -0400

    created a server

---
+++ routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/ping" , (req, res, next) => {
+  res.status(200);
+  res.send({"success" : true});
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const port  = 3000;
+    // MIDDLEWARE 
+    app.options("*" , cors());
+    app.use(cors());
+    app.use(express.json());
+    app.use('/' , express.static('public'));
+    app.use(bodyParser.json())
+    
+    //Routes
+    const router = require('./routes')
+    app.use('/api' , router)
+    
+    
+    
+    
+    
+    
+    
+    // SERVER LAUNCH
+    app.listen(process.env.PORT || port, () => {
+      console.log("listening to port" + (port));
+    })
