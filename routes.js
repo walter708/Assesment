34aadc3311cf69c3fc69b6bcdc2308fa299e748a
unknown <Walter Okpala>
2022-05-24 16:06:17 -0400

    made a function to fetch posts

--- routes.js
+++ routes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {fetchData , sort}= require('./helper')
+const apicache = require("apicache");
 
 router.get("/ping" , (req, res, next) => {
   res.status(200);
   res.send({"success" : true});
 })
 
-router.get("/posts" , async (req, res, next) => {
+const cache = apicache.middleware;
+
+router.get("/posts" ,cache("60 minutes"), async (req, res, next) => {
   const [key , data] = await fetchData(req.query)
       if(key === 400){
         res.status(400).json({ error: String(data)})
