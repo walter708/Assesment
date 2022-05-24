1ea4f19c73a7334502dae60823dd8a280caac6b8
unknown <Walter Okpala>
2022-05-24 14:57:59 -0400

    made a function to fetch posts

--- routes.js
+++ routes.js
@@ -11,14 +11,12 @@ router.get("/posts" , async (req, res, next) => {
   const [key , data] = await fetchData(req.query)
   
   if(key === 400){
-    res.status(400).send(data)
+    res.status(400).json({ error: String(data)})
   }else if(key === 500){
-    res.status(500).send(data)
+    res.status(500).json({ error: String(data)})
   }else{
-    res.status(200).send(data)
+    res.status(200).send({"posts":data})
   }
-  
-
   
   
   
